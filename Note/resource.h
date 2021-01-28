72f2623b6b2c4f2f83480190962049400516a030
ivankrtv <mega.krotov@mail.ru>
2021-01-28 15:45:03 +0300

    Ready version

---
+++ Note/resource.h
@@ -0,0 +1,40 @@
+//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется Note.rc
+//
+#define IDC_MYICON                      2
+#define IDD_NOTE_DIALOG                 102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_NOTE                        107
+#define IDI_SMALL                       108
+#define IDC_NOTE                        109
+#define IDR_MAINFRAME                   128
+#define IDI_ICON1                       134
+#define IDI_ICON                        134
+#define IDD_DIALOG1                     136
+#define IDR_ACCELERATOR1                138
+#define IDC_MFCSHELLTREE1               1000
+#define ID_Menu                         32777
+#define ID_32791                        32791
+#define ID_32792                        32792
+#define ID_Menu32793                    32793
+#define ID_32794                        32794
+#define WM_CUT                          32795
+#define ID_33333                        32796
+#define ID_32793                        32797
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        161
+#define _APS_NEXT_COMMAND_VALUE         32806
+#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
